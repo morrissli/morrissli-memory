ca65ed4fa57528095c0433fe6818e5a34910480d
David <135055475+morrissli@users.noreply.github.com>
2024-05-13 13:28:07 +0300

    Add files via upload

---
+++ 1/script.js
@@ -0,0 +1,15 @@
+console.log(typeof 42);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof undefined);
+
+var age = 16;
+let quanfity = 25;
+const MAX_VALUME = 500;
+console.log(`${age} ${quanfity} ${MAX_VALUME}`);
+
+const a = 3, b = 7;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
