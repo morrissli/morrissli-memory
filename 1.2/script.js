f5052197faacf5aa2fdc0facdec499193dd5888d
David <135055475+morrissli@users.noreply.github.com>
2024-05-13 13:36:22 +0300

    Add files via upload

---
+++ 1.2/script.js
@@ -0,0 +1,5 @@
+const inputUser1 = prompt("Введіть перший рядок:");
+const inputUser2 = prompt("Введіть другий рядок:");
+const inputUser3 = prompt("Введіть третій рядок:");
+
+console.log(`${inputUser1} ${inputUser2} ${inputUser3}`)
