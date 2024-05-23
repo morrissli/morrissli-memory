5fdfb3febb2fa08dec5569ce9d237c35ac306887
David <135055475+morrissli@users.noreply.github.com>
2024-05-23 13:52:45 +0300

    Add files via upload

--- 5.2/script.js
+++ 5.2/script.js
@@ -1,5 +1,6 @@
-for (let i = 26; i <= 26; i += 10) {
-    for (let j = 10; j <= 100; j += 10) {
-        console.log(`${i} * ${j} = ${i * j}`);
-    }
-}
+const dollar = 26;
+
+for (let i = 10; i <= 100; i += 10) {
+    const ua = i * dollar;
+    console.log(`${i} долларів = ${ua} гривень`);
+};
